7ccebb7cdf6f191fa4224ae306b1747b2afa3034
crooruhe <cruhe@yahoo.com>
2023-05-11 20:57:58 +0000

    party time

---
+++ github_script.sh
@@ -0,0 +1,11 @@
+eval "$(ssh-agent -s)"
+sleep 2
+cd /home/code/github_green_squares
+varOne=$RANDOM
+echo $varOne
+echo $VarOne >> README.md
+ssh-add ~/.ssh/github.keygen
+sleep 1
+git add *
+git commit -m "$varOne"
+git push git@github.com:crooruhe/github_green_squares.git
